c8d4b699d2aabe904cf5ef35e3b92e05f708ec46
Joao Pedro Cardoso <joaopedrocar@hotmail.com>
2018-10-08 09:53:02 -0300

    Initial Commit

---
+++ alura-travels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alura-travels
+//
+//  Created by João Pedro Cardoso de Araújo on 08/10/18.
+//  Copyright © 2018 João Pedro Cardoso de Araújo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ alura-travelsTests/alura_travelsTests.swift
@@ -0,0 +1,34 @@
+//
+//  alura_travelsTests.swift
+//  alura-travelsTests
+//
+//  Created by João Pedro Cardoso de Araújo on 08/10/18.
+//  Copyright © 2018 João Pedro Cardoso de Araújo. All rights reserved.
+//
+
+import XCTest
+@testable import alura_travels
+
+class alura_travelsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
